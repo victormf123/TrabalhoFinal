662a51813b0e4cec990fc9409aab0b7ffe267208
victormf123 <victormf.df@hotmail.com>
2018-10-24 04:18:16 -0300

    Primeiro commit versão 1 resolução de bugs

---
+++ TrabalhoFinal/ListaCelula.swift
@@ -0,0 +1,17 @@
+//
+//  ListaCelula.swift
+//  TrabalhoFinal
+//
+//  Created by Matheus Freitas on 24/10/18.
+//  Copyright © 2018 Matheus Freitas. All rights reserved.
+//
+
+import UIKit
+
+class ListaCelula: UITableViewCell{
+    
+    @IBOutlet weak var titulo: UILabel!
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var videogames: UIImageView!
+    @IBOutlet weak var computador: UIImageView!
+}
